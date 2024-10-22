15ee229b9dacd95e388d88330b5ae92d5828fdfb
rgarmashev91 <rgarmashev91@gmail.com>
2024-10-22 17:35:00 +0300

    Update Task_1.py
    Исправление: замена поиска элемента списка через цикл на поиск при помощи метода index()

--- Lab_1/Task_1.py
+++ Lab_1/Task_1.py
@@ -2,10 +2,7 @@ numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -
 
 # TODO заменить значение пропущенного элемента средним арифметическим
 
-index = 0
-
-while numbers[index] is not None:
-    index += 1
+index = numbers.index(None)
 
 sum_of_numbers = sum(numbers[:index]) + sum(numbers[index + 1:])
 count_of_numbers = len(numbers)
