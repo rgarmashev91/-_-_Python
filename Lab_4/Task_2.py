0b5498ee02ee15479a12d033f3555ad1da16fb39
rgarmashev91 <rgarmashev91@gmail.com>
2024-11-17 20:56:57 +0300

    Lab_4

---
+++ Lab_4/Task_2.py
@@ -0,0 +1,24 @@
+# TODO импортировать необходимые молули
+import csv
+import json
+
+
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+
+
+def task() -> None:
+    with open(INPUT_FILENAME) as file:  # TODO считать содержимое csv файла
+        csv_reader = csv.DictReader(file)
+        data = [row for row in csv_reader]
+    with open(OUTPUT_FILENAME, 'w') as file:  # TODO Сериализовать в файл с отступами равными 4
+        json.dump(data, file, indent=4)
+
+
+if __name__ == '__main__':
+    # Нужно для проверки
+    task()
+
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
